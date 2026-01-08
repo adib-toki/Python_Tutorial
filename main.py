55867d14913fe47664a36efd06728e26b5b5ec1d
adib-toki <adib.toki.pc@gmail.com>
2026-01-08 14:42:08 +0600

    fixed

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-name = "Adib"
-print(name)
-age = 15
-print(age)
+#Int Type Data
+adib = 15
+print(type(adib))
+
+#Float Type Data
+asif = 58.2
+print(type(asif))
+
+#Complex Type Data
+iqubal = 420j
+print(type(iqubal))
