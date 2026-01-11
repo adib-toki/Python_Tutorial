dfec0d468eb2640271bac2ec642b2c3c4492afa0
adib-toki <adib.toki.pc@gmail.com>
2026-01-11 13:29:30 +0600

    modify

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-#String Formatting
-a = 10
-b = 60
-print(f"This is my super number {a+b} & {b-a}")
-name = "Adib"
-roll = 7
-print(f"My name is {name} & and my Class Roll {roll}")
+#Binary Type
+a = [1,2,3,4,5,6,7,8,9,255]
+b = bytes(a)
+print(type(b))
+print(b[1])
+
+c = [1,2,4,4,5,6,7,8,9,255]
+d = bytearray(c)
+print(type(b))
+d[2] = 3
+print(d[2])
