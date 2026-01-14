3347aaa66fccee50449bcb00c889a3b917d583cc
adib-toki <adib.toki.pc@gmail.com>
2026-01-14 22:35:29 +0600

    error

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-#Binary Type
-a = [1,2,3,4,5,6,7,8,9,255]
-b = bytes(a)
-print(type(b))
-print(b[1])
-
-c = [1,2,4,4,5,6,7,8,9,255]
-d = bytearray(c)
-print(type(b))
-d[2] = 3
-print(d[2])
+#None Type
+x = None
+print(x)
+print(type(x))
