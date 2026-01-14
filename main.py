ed1da513b3536ba99a6854bdd162575e4906762d
adib-toki <adib.toki.pc@gmail.com>
2026-01-14 22:53:42 +0600

    init

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-#None Type
-x = None
-print(x)
-print(type(x))
+#Sequence Type
+li = ["asif","iqubal","adib","PNS"]
+print(li)
+li[3] = "MZS"
+print(li)
+print(type(li))
+
+tu = ("A","I","Adib")
+print(tu)
+print(type(tu))
+
+ran = range(6)
+print(ran)
+print(type(ran))
