4d5857260b20ee287a93d56ec8c11c81f44e6896
adib-toki <adib.toki.pc@gmail.com>
2026-01-17 14:07:01 +0600

    init

--- main.py
+++ main.py
@@ -1,43 +1,6 @@
-#Operators
-a = 10
-b = 20
-
-print(a+b)
-
-print(b-a)
-
-print(a*b)
-
-print(b/a)
-
-print(a % b)
-
-print(a ** b)
-
-print(a // b)
-
-
-z = 5
-sum1 = z + 5
-print(sum1)
-
-c = 10
-c += 10
-print(c)
-
-c -= 5
-print(c)
-
-
+#Swapping
 x = 10
-print(x == 30)
-
-print(x != 20)
-
-print(x > 50)
-
-print(x < 50)
-
-print(x <= 5)
-
-print(x >= 10)
+y = 2
+x,y = y,x
+print("This value is now x = ",x)
+print("This value is now y = ",y)
