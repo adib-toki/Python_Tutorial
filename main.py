a9fc513af8f5f088bf0b714cbceb8e030620454d
adib-toki <adib.toki.pc@gmail.com>
2026-01-19 11:56:14 +0600

    update: improve overall project

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-#Type Casting
-name = "12"
-print(type(name))
-print(type(int(name)))
+#Python Lists
+li = [1,2,3,4,5,6,7]
+print(li)
+print(li[2])
 
-num1 = 50
-print(num1)
-print(type(num1))
-print(float(num1))
-print(type(num1))
+a = ["apple", "banana", "cherry", "apple", "cherry"]
+print(a)
+a[3] = "watermelon"
+print(a)
+
+b = [True,False,True,False,True]
+print(b)
+print(type(b))
