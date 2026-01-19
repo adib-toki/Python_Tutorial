4280c9b37d06dc3fca2af84c7d1ab5c47fd11136
adib-toki <adib.toki.pc@gmail.com>
2026-01-19 11:41:45 +0600

    fixed

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-#User Input
-a = input("Enter Your username : ")
-b = input("Enter Your Password : ")
-print(a)
-print(b)
+#Type Casting
+name = "12"
+print(type(name))
+print(type(int(name)))
+
+num1 = 50
+print(num1)
+print(type(num1))
+print(float(num1))
+print(type(num1))
