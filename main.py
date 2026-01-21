db6e587542b91a5e3db8ec20ac0524a7d1285952
adib-toki <adib.toki.pc@gmail.com>
2026-01-21 14:12:17 +0600

    small improvements

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-#Join Two List
-num1 = [1,2,3,4,5,"Adib"]
-num2 = [6,7,8,9,0,"MZS"]
-
-num3 = num1 + num2
-print(num3)
-
-num1.extend(num2)
-print(num1)
+#Matrix
+li = [
+    [1,2,3,4,5],
+    [6,7,8,9,10],
+    "Adib"
+]
+print(li[1][1])
