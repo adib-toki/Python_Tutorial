1e9ba61f3d56a54e36399bac7e50f965cb8da853
adib-toki <adib.toki.pc@gmail.com>
2026-01-26 13:41:30 +0600

    feat: add authentication logic

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-#Join Tuple
-x = ("apple", "banana", "cherry","kiwi")
-y = ("Strawberry","Watermelon","Grapes")
+#Tuple Method
+num1 = (1,2,3,4,5,5,2,7,9,0,1,2,2,2,6)
 
-print(x+y)
+print(num1.index(5))
 
-a = x+y
-print(a)
-
-b = x*2
-print(b)
+print(num1.count(2))
