f9ee6e273d854aa41a780e133a3b17bbdcc18dfa
adib-toki <adib.toki.pc@gmail.com>
2026-01-29 13:26:09 +0600

    fix: fix ZeroDivisionError

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-#Access Set Item
-a = {1,2,3,4,5,6,7,8,9}
+#Add Set Item
+thisset = {"apple", "banana", "cherry"}
 
-for b in a:
-    print(b)
+thisset.add("orange")
+print(thisset)
 
-print(7 in a)
+
+x = {"apple", "banana", "cherry"}
+y = {"pineapple", "mango", "papaya"}
+
+x.update(y)
+print(x)
+
+
+set1 = {1,2,3,4,5}
+list1 = [6,7,8,9,0]
+
+set1.update(list1)
+print(set1)
